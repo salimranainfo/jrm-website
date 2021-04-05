f222b936c5d45597e82d0bb0e6e05a759aabaa64
Muhammad-Rubel <rubel.contact@gmail.com>
2021-04-05 18:31:31 +0600

    Tab funtionality added

---
+++ src/components/product/Knit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- Card Container -->
+    <div class="flex justify-center items-center space-x-2">
+      <div>
+        <img
+          src="../../assets/product_image/knit.jpg"
+          alt=""
+          class="h-20 w-48"
+        />
+      </div>
+      <div>
+        <p>Product Name: Knit</p>
+        <p>Fabrication</p>
+        <p>MOQ</p>
+        <p>Lead Time</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Knit'
+}
+</script>
+
+<style></style>
